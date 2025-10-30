a95f331bce82ec644b4b8056b2137597e80addee
Pradeep-Nexcode <pradeep.nexcode@gmail.com>
2025-10-30 07:17:46 +0530

    contact model added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import Script from "next/script";
 import { Navbar, Footer, OurWorks, Blogs } from "@/components";
-import { ContactModalProvider  } from "../components/forms/ContactModal";
+import { ContactModalProvider } from "../components/forms/ContactModal";
 
 const nunito = Nunito({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,23 +25,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${nunito.variable} antialiased`}>
-
         <ContactModalProvider>
-        <Navbar />
-        {children}
+          <Navbar />
+          {children}
 
-        <Footer />
+          <Footer />
         </ContactModalProvider>
-
-        {/* ✅ Load JS dependencies safely and globally
-        <Script src="/js/imagesloaded.pkgd.min.js" strategy="beforeInteractive" />
-        <Script src="/js/three.min.js" strategy="beforeInteractive" />
-        <Script src="/js/TweenLite.min.js" strategy="beforeInteractive" />
-        <Script src="/js/CSSPlugin.min.js" strategy="beforeInteractive" />
-        <Script src="/js/EasePack.min.js" strategy="beforeInteractive" />
-        <Script src="/js/GridToFullscreenEffect.js" strategy="beforeInteractive" />
-        <Script src="/js/basicDemo.js" strategy="afterInteractive" /> */}
       </body>
-    </html>
+    </html> 
   );
 }
